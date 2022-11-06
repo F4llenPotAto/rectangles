f1b0561f454bd7fd50e444e1dbcfa1c7c2e9c325
Trenton Ornelas <trentonornelas@gmail.com>
2022-11-06 12:23:02 -0600

    Add values to enum IpAddrKind

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 enum IpAddrKind {
-    V4,
-    V6,
+    V4(String),
+    V6(String),
 }
 
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
+    let home = IpAddrKind::V4(String::from("127.0.0.1"));
+
+    let loopback = IpAddrKind::V6(String::from("::1"));
 
     route(IpAddrKind::V4);
     route(IpAddrKind::V6);
