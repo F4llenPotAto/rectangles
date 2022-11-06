24d7a29a997c622e421f320aceb4189e704df6e5
Trenton Ornelas <trentonornelas@gmail.com>
2022-11-06 12:21:37 -0600

    Create an enum representing ipv4 and v6

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,14 @@
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-
-    fn square(size: u32) -> Self {
-        Self {
-            width: size,
-            height: size,
-        }
-    }
-}
-
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-}
-
-
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
+enum IpAddrKind {
+    V4,
+    V6,
 }
 
 fn main() {
-    let sq = Rectangle::square(3);
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
 
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-    let rect3 = Rectangle {
-        width: 60,
-        height: 45,
-    };
-
-    println!(" Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!(" Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
-    println!(" Use of an associated function, square, with value {:#?}", sq);
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
 }
+
+fn route(ip_kind: IpAddrKind) {}
