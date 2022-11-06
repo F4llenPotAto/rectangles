56aaf6238a16f201e21ab38c88a20487fe510f26
Trenton Ornelas <trentonornelas@gmail.com>
2022-11-06 11:13:27 -0600

    Add in associated function, square

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,24 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 fn main() {
+    let sq = Rectangle::square(3);
+
     let rect1 = Rectangle {
         width: 30,
         height: 50,
@@ -28,4 +38,5 @@ fn main() {
 
     println!(" Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!(" Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!(" Use of an associated function, square, with value {:#?}", sq);
 }
